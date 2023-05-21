a129f310756661b29d2b48d32aaf9080a3530c54
habib-rahmanhabib <mdhabiburrahman80680@gmail.com>
2023-05-21 23:50:33 +0600

    some update on server

--- index.js
+++ index.js
@@ -25,13 +25,18 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+   
     const toysCollection = client.db('ToyCars').collection('Toys')
 
     app.get('/toys', async (req, res) => {
       const cursor = toysCollection.find()
       const result = await cursor.toArray()
+      res.send(result)
+    })
+    app.get('/toys/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await toysCollection.findOne(query)
       res.send(result)
     })
 
